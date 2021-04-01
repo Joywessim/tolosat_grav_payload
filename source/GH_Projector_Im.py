7b5d49183d15d941bf3296bb8ab878bac1a4fff8
Wessim omezzine <63546883+Joywessim@users.noreply.github.com>
2021-04-01 19:57:00 +0200

    Delete GH_Projector_Im.py

--- source/GH_Projector_Im.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import numpy.linalg as npl 
-def Projector_im(A):
-    #This function defines the projection operator on the orthogonal space of the Image space of a matrix A
-    
-    m,n=np.shape(A) 
-    return np.eye(n)- np.dot(A, npl.inv(np.dot(A.T,A)).dot(A.T))
